43ddde6b81777068e367d857eb1f03e6d5d79856
redxfree <100827526+redxfree@users.noreply.github.com>
2022-08-22 14:32:57 +0530

    commit

--- Article/Article/settings.py
+++ Article/Article/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -172,3 +173,4 @@ REST_USE_JWT = False
 SITE_ID = 1
 
 
+django_heroku.settings(locals())
